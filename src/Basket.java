5927835d7141cbf5cedaf4a9b1d206d643dd1e6b
Andy Verh <bepx@inbox.ru>
2022-11-17 20:39:23 +0600

    результат работы записи/чтения через встроенную сериализацию

--- src/Basket.java
+++ src/Basket.java
@@ -1,9 +1,8 @@
 import java.io.*;
-import java.util.List;
 
 import static java.lang.System.out;
 
-public class Basket {
+public class Basket implements Serializable {
 
     protected int[] prices;
     protected String[] nameProduct;
@@ -16,74 +15,26 @@ public class Basket {
 
     }
 
-    public void addToCart(int productNumber, int amount) {  //номер продукта, штук продукта
-        prodAmount[productNumber] = amount;
-        //prices[productNumber] = prices[productNumber]* amount;
+    public void saveBin(File file) throws Exception {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                oos.writeObject(this);
 
-    }
-
-    public void printCart(int productNumber, int amount) {
-        out.println("В корзину добавлено: " + amount + " шт. " + nameProduct[productNumber] + " - " +
-                prices[productNumber] + "руб./шт, на сумму - " + prices[productNumber] * amount + " руб.");
-    }
-
-    public void saveTxt(File textFile) throws IOException {
-
-        try (PrintWriter out = new PrintWriter(textFile)) {
-            for (int e : prices) {
-                out.write(e + "@");
-            }
-            out.write("\n");
-            for (String s : nameProduct) {
-                out.write(s + "@");
-            }
-            out.write("\n");
-            for (int i : prodAmount) {
-                out.write(i + "@");
-            }
         } catch (IOException e) {
-            e.printStackTrace();
+            out.println(e.getMessage());
         }
     }
 
-    static Basket loadFromTxtFile(File textFile) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(textFile))) {
-            //считываем построчно данные, переданные текстФайлом
-            String line1 = reader.readLine();
-            String line2 = reader.readLine();
-            String line3 = reader.readLine();
+    static Basket loadFromBinFile(File file) throws Exception {
+        Basket basket = null;
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            basket = (Basket) ois.readObject();
 
-            //Создание массива строк из 1й строки и перевод его значений в число
-            String[] exeptedPriceArr = line1.split("@");
-            int[] exeptedPriceArrInt = new int[exeptedPriceArr.length];
-            for (int i = 0; i < exeptedPriceArr.length; i++) {
-                exeptedPriceArrInt[i] = Integer.parseInt(exeptedPriceArr[i]);
-            }
-            //Создание массива строк из 2й строки
-            String[] exeptedNameProduct = line2.split("@");
-
-            //Создание массива строк из 3й строки и перевод его значений в число
-            String[] exeptedCountProduct = line3.split("@");
-            int[] exeptedCountProductInt = new int[exeptedCountProduct.length];
-            for (int i = 0; i < exeptedCountProduct.length; i++) {
-                exeptedCountProductInt[i] = Integer.parseInt(exeptedCountProduct[i]);
-            }
-
-            //Вывод на экран всех строк, извлеченных из файла
-            for (String s : exeptedPriceArr) {
-                System.out.print(s + " руб./шт., ");
-            }
-            System.out.print("\n");
-            for (String s : exeptedNameProduct) {
-                System.out.print(s + ", ");
-            }
-            System.out.print("\n");
-            for (String s : exeptedCountProduct) {
-                System.out.print(s + " шт., ");
-            }
-            return new Basket(exeptedPriceArrInt, exeptedNameProduct, exeptedCountProductInt);
-        } catch (IOException e) {
-            throw new RuntimeException();
+        } catch (Exception e) {
+            out.println(e.getMessage());
         }
+        return basket;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,16 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         String[] listOfProducts = {"Хлеб", "Яблоки", "Молоко 2,5%"};
         int[] prices = {40, 95, 49}; // 0 - 40, 1 - 95, 2 - 49
         int[] countIndex = new int[prices.length]; //массив хранения количества товара по позициям (индексам)
-        int[] sumIndex = new int[prices.length]; //массив хранения общей суммы по позициям
 
         System.out.println("Список возможных продуктов:");
         for (int i = 0; i < listOfProducts.length; i++) {
             System.out.println((i + 1) + ". " + listOfProducts[i] + " - " + prices[i] + " руб./шт");
         }
-
-        //int sumProducts = 0; //итоговая сумма продуктов
-        int productNumber = 0;
-        int productCount = 0;
-
 
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -35,32 +29,12 @@ public class Main {
                 break;
             }
 
-            String[] parts = input.split(" ");
-            productNumber = Integer.parseInt(parts[0]) - 1; //это числовой индекс продукта
-            productCount = Integer.parseInt(parts[1]); //это множитель количества этого продукта
-
             Basket basket = new Basket(prices, listOfProducts, countIndex);
-            basket.addToCart(productNumber, productCount);
-            basket.printCart(productNumber, productCount);
-            basket.saveTxt(new File("basket.txt"));
+            basket.saveBin(new File ("basket.bin"));
 
-            System.out.println();
-            System.out.println("Извлечение из файла");
-            Basket.loadFromTxtFile(new File("basket.txt"));
-
+            Basket.loadFromBinFile(new File ("basket.bin"));
         }
     }
-
-//        System.out.println();
-//        System.out.println("Ваша корзина:");
-//
-//        for (int i = 0; i < listOfProducts.length; i++) {
-//            System.out.println(listOfProducts[i] + " " + countIndex[i] + " шт." + " " + prices[i] + " руб./шт " + sumIndex[i] + " р. в сумме");
-//            sumProducts += sumIndex[i];
-//        }
-//
-//        System.out.println("Итого: " + sumProducts + " руб.");
-
 }
 
 
